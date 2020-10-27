d711c0e0d6393d9bf45884f8a341b9c8e0372061
Paula Caceres <paulacaceres.dis@gmail.com>
2020-10-26 21:11:53 -0300

    deploy

---
+++ build/precache-manifest.94ff46a9666c45802d0fe750de0e7bbd.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd811b3e98bd9caba3cc5bb96319a6da",
+    "url": "/dropped-time-store/index.html"
+  },
+  {
+    "revision": "8896216a6e13f51e094d",
+    "url": "/dropped-time-store/static/css/main.b79335f5.chunk.css"
+  },
+  {
+    "revision": "44c52d99e0ef0abc2c75",
+    "url": "/dropped-time-store/static/js/2.4beba1d1.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/dropped-time-store/static/js/2.4beba1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8896216a6e13f51e094d",
+    "url": "/dropped-time-store/static/js/main.f069435b.chunk.js"
+  },
+  {
+    "revision": "5f45f677a8eba6979847",
+    "url": "/dropped-time-store/static/js/runtime-main.869a1d87.js"
+  },
+  {
+    "revision": "a998c655d7610f69d2f583e4958bc69d",
+    "url": "/dropped-time-store/static/media/Basier-Circle-Bold-Italic.a998c655.otf"
+  },
+  {
+    "revision": "5506c555ae8cb4f9d2b126cafea97282",
+    "url": "/dropped-time-store/static/media/Basier-Circle-Bold.5506c555.otf"
+  },
+  {
+    "revision": "3f3eddff7ad7b08b1c9f2e29d677e75f",
+    "url": "/dropped-time-store/static/media/Basier-Circle-Medium-Italic.3f3eddff.otf"
+  },
+  {
+    "revision": "d0b4b809e9c8dbb376786c71658d7510",
+    "url": "/dropped-time-store/static/media/Basier-Circle-Regular-Italic.d0b4b809.otf"
+  },
+  {
+    "revision": "4dc9ed9540bc51de7b9d678693ef7692",
+    "url": "/dropped-time-store/static/media/Basier-Circle-SemiBold-Italic.4dc9ed95.otf"
+  },
+  {
+    "revision": "12c82e5fadd21708682985dae09afb7d",
+    "url": "/dropped-time-store/static/media/Basier-Circle-SemiBold.12c82e5f.otf"
+  },
+  {
+    "revision": "7dfdb9414ad1b37b5570386ec86a535b",
+    "url": "/dropped-time-store/static/media/Basier-Square-Bold-Italic.7dfdb941.otf"
+  },
+  {
+    "revision": "19e4e6d041c60fa4949e8fb019ccdeef",
+    "url": "/dropped-time-store/static/media/Basier-Square-Bold.19e4e6d0.otf"
+  },
+  {
+    "revision": "85982094491f3ee9bce09f1414d57c00",
+    "url": "/dropped-time-store/static/media/Basier-Square-Medium-Italic.85982094.otf"
+  },
+  {
+    "revision": "03326b206c0ba454a745f9f1c4363126",
+    "url": "/dropped-time-store/static/media/Basier-Square-Medium.03326b20.otf"
+  },
+  {
+    "revision": "4671edf2345fa969e9e20035d07e42d5",
+    "url": "/dropped-time-store/static/media/Basier-Square-Regular-Italic.4671edf2.otf"
+  },
+  {
+    "revision": "ff4d8560467c9d5185b1f01b5617bba4",
+    "url": "/dropped-time-store/static/media/Basier-Square-Regular.ff4d8560.otf"
+  },
+  {
+    "revision": "ca526f1ecc9645b37c8067f69297829b",
+    "url": "/dropped-time-store/static/media/Basier-Square-SemiBold-Italic.ca526f1e.otf"
+  },
+  {
+    "revision": "d322396c94c3ba1103ddd07ea10dec16",
+    "url": "/dropped-time-store/static/media/Basier-Square-SemiBold.d322396c.otf"
+  },
+  {
+    "revision": "4d3169a0be4814396241516eec7cea98",
+    "url": "/dropped-time-store/static/media/Candelion-bold.4d3169a0.otf"
+  },
+  {
+    "revision": "4d3169a0be4814396241516eec7cea98",
+    "url": "/dropped-time-store/static/media/Candelion-bold2.4d3169a0.otf"
+  },
+  {
+    "revision": "c6583f5cf02215793effbf309c5ccbd6",
+    "url": "/dropped-time-store/static/media/Candelion-regular.c6583f5c.otf"
+  },
+  {
+    "revision": "c6583f5cf02215793effbf309c5ccbd6",
+    "url": "/dropped-time-store/static/media/Candelion-regular2.c6583f5c.otf"
+  },
+  {
+    "revision": "808e557f8d8c95879076472466362839",
+    "url": "/dropped-time-store/static/media/Candelion-rough.808e557f.otf"
+  },
+  {
+    "revision": "808e557f8d8c95879076472466362839",
+    "url": "/dropped-time-store/static/media/Candelion-rough2.808e557f.otf"
+  },
+  {
+    "revision": "9564480a9c8dd9dfcb67395d0a03ea2a",
+    "url": "/dropped-time-store/static/media/favicon-dropped.9564480a.svg"
+  },
+  {
+    "revision": "c96fe19d1d2cf0f468641f9e7284cf90",
+    "url": "/dropped-time-store/static/media/feature_product1.c96fe19d.jpg"
+  },
+  {
+    "revision": "8b4d86856004c5a2ebfc96a21ff51cff",
+    "url": "/dropped-time-store/static/media/feature_product2.8b4d8685.jpg"
+  },
+  {
+    "revision": "8dc6a84ee46a6e88718887e7099cd002",
+    "url": "/dropped-time-store/static/media/icon-facebook.8dc6a84e.svg"
+  },
+  {
+    "revision": "e1512045269c10cfcc29d30376b1c45c",
+    "url": "/dropped-time-store/static/media/icon-instagram.e1512045.svg"
+  },
+  {
+    "revision": "85e271b3aa71e0f18aeef1645793b77c",
+    "url": "/dropped-time-store/static/media/icon-linkedin.85e271b3.svg"
+  },
+  {
+    "revision": "47a26b262edeb83a15df50990c063fee",
+    "url": "/dropped-time-store/static/media/icon-twitter.47a26b26.svg"
+  },
+  {
+    "revision": "ee83316a9d67abff13b2b6300dbe5be2",
+    "url": "/dropped-time-store/static/media/logo-dropped.ee83316a.svg"
+  },
+  {
+    "revision": "e212323c8b808436aeb6a5484e93c061",
+    "url": "/dropped-time-store/static/media/shopping.e212323c.png"
+  },
+  {
+    "revision": "027372bf495b11ea60e54b2eaf2007cf",
+    "url": "/dropped-time-store/static/media/test.027372bf.jpg"
+  }
+]);
